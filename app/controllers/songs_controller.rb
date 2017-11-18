88bbf3b1ee276527159797a4b8115f9c9aab0ce9
Roel de Jong <Roel4811@hotmail.com>
2017-11-18 18:25:50 +0100

    Add songs controller

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+
+  def index
+  end
+
+  def create
+    @song = Song.new(song_params)
+    respond_to do |format|
+      format.json do
+        if @song.save
+          flash[:notice] = "Thank you!"
+          render json: @song
+        else
+          flash[:notice] = "Did't work"
+          render json: { errors: @song.errors.messages }, status: 422
+        end
+      end
+    end
+  end
+
+  private
+
+    def song_params
+      # params.fetch(:song, {}).permit(:name, :lyrics)
+      params.require(:song).permit(:lyrics, :name, :artist_id)
+    end
+end
