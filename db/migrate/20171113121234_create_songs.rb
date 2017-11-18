4d5373abdb42991c843034e8aed8eba154a540c1
Roel de Jong <Roel4811@hotmail.com>
2017-11-18 18:24:59 +0100

    Create songs

--- db/migrate/20171113121234_create_songs.rb
+++ db/migrate/20171113121234_create_songs.rb
@@ -1,6 +1,6 @@
 class CreateSongs < ActiveRecord::Migration[5.1]
   def change
-    create_table :songs do |t|
+    create_table :songs, id: :integer do |t|
       t.string :name
       t.text :lyrics
 
