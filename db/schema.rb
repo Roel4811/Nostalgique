67a234c7e10588bc901c58892920d41586c6c4d5
Roel de Jong <Roel4811@hotmail.com>
2017-12-12 17:23:35 +0000

    Add bootstrap and image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208173708) do
+ActiveRecord::Schema.define(version: 20171212162650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171208173708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "artist_id"
+    t.string "image"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
