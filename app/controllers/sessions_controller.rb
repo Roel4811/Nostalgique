b469432aee10bdafc48c2c6f4711de85a5d62bcb
Roel de Jong <Roel4811@hotmail.com>
2017-12-24 01:55:53 +0100

    Add member session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    member = Member.find_by(email: params[:email])
+    if member && member.authenticate(params[:password])
+      session[:member_id] = member.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:member_id] = nil
+    redirect_to '/login'
+  end
+end
