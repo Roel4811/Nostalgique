2b5dc09e9038a61f7fdb9b78894d9933134b5081
Roel de Jong <Roel4811@hotmail.com>
2018-01-01 14:35:34 +0100

    Update session redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,18 @@ class SessionsController < ApplicationController
     member = Member.find_by(email: params[:email])
     if member && member.authenticate(params[:password])
       session[:member_id] = member.id
-      redirect_to '/'
+      url = session[:return_to] || root_path
+      session[:return_to] = nil
+      url = root_path if url.eql?(logout_session_path)
+      puts "URL to redirect to: #{url}"
+      redirect_to url
     else
-      redirect_to '/login'
+      redirect_to new_session_path
     end
   end
 
   def destroy
     session[:member_id] = nil
-    redirect_to '/login'
+    redirect_to new_session_path
   end
 end
