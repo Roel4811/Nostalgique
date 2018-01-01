4eab2b146ea5fa39a9a17e4e21d560f129a50a7d
Roel de Jong <Roel4811@hotmail.com>
2018-01-01 14:38:05 +0100

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219192945) do
+ActiveRecord::Schema.define(version: 20171224115928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,8 +74,11 @@ ActiveRecord::Schema.define(version: 20171219192945) do
     t.bigint "artist_id"
     t.string "image"
     t.text "lyrics_en"
+    t.integer "member_id"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
+    t.index ["member_id"], name: "index_songs_on_member_id"
   end
 
   add_foreign_key "songs", "artists"
+  add_foreign_key "songs", "members"
 end
