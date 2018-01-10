cb0c7f7d895686e19831648ad3e5bd60c372862f
Roel de Jong <Roel4811@hotmail.com>
2018-01-10 21:39:04 +0100

    Update associations

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,11 @@
 class Member < ApplicationRecord
   has_many :shopping_baskets
+  has_many :songs
 
   has_secure_password
 
   def whole_name
     "#{self.first_name} #{self.last_name}"
   end
-  
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,11 @@ class Song < ApplicationRecord
   validates :lyrics, presence: true
 
   belongs_to :artist
+  belongs_to :member
 
   mount_uploader :image, ImageUploader
+
+  def current_step
+    @current_step || Wizard::Song::STEPS.first
+  end
 end
