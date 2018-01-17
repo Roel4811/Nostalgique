26d64bfd451d68fccd4d52756b917eacc1ebdbf4
Roel de Jong <Roel4811@hotmail.com>
2018-01-17 15:58:46 +0100

    Add strong params to session log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    member = Member.find_by(email: params[:email])
-    if member && member.authenticate(params[:password])
+    member = Member.find_by(email: session_params[:email])
+    if member && member.authenticate(session_params[:password])
       session[:member_id] = member.id
       url = session[:return_to] || root_path
       session[:return_to] = nil
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
       puts "URL to redirect to: #{url}"
       redirect_to url
     else
+      puts "failed to log-in"
       redirect_to new_session_path
     end
   end
@@ -20,5 +21,11 @@ class SessionsController < ApplicationController
   def destroy
     session[:member_id] = nil
     redirect_to new_session_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
