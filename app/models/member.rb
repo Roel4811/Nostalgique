2eda07d5cf15bd45fbffdc64234971935385dab7
Roel de Jong <Roel4811@hotmail.com>
2018-01-22 23:22:12 +0100

    Add login to members

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,10 @@ class Member < ApplicationRecord
 
   has_secure_password
 
+  def send_password_reset
+    MemberMailer.password_reset(self).deliver
+  end
+
   def whole_name
     "#{self.first_name} #{self.last_name}"
   end
