3d61e0a967305dc57c789e2a565b7ec9718a5d2d
Roel de Jong <Roel4811@hotmail.com>
2018-01-22 17:49:18 +0100

    Create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120170952) do
+ActiveRecord::Schema.define(version: 20180122110412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,16 @@ ActiveRecord::Schema.define(version: 20180120170952) do
     t.integer "status", default: 0
     t.index ["artist_id"], name: "index_songs_on_artist_id"
     t.index ["member_id"], name: "index_songs_on_member_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "role"
+    t.string "password_digest"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "songs", "artists"
