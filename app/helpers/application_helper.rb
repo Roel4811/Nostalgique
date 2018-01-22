40de2f537ca5f1e17753999a8f640694a4d71528
Roel de Jong <Roel4811@hotmail.com>
2018-01-22 17:49:34 +0100

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_msgs
+    hash = { 'notice' => 'success', 'alert' => 'warning', 'error' => 'danger' }
+
+    string = ''
+    flash.each do |name, msg|
+      string += content_tag(:div, class: "alert alert-#{hash[name]}") do
+        '<a class="close" data-dismiss="alert">x</a>'.html_safe +
+        msg
+      end
+    end
+    string.html_safe
+  end
 end
