acf68384c882f96e128d79cb7934da66d2585bb8
Roel de Jong <Roel4811@hotmail.com>
2018-01-25 17:32:23 +0100

    Write test to fetch song

--- spec/requests/spotify_api_spec.rb
+++ spec/requests/spotify_api_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe "GetTrackFromSpotify", type: :request do
   it "save spotify track image to song" do
     member = FactoryGirl.create(:member)
     visit root_path
-    fill_in "email", with: member.Email
-    fill_in "password", with: "secret"
+    click_link "Add Song"
+    fill_in "Email", with: member.email
+    fill_in "Password", with: "secret"
     click_button "Log in"
 
-    click_link "add song"
-    fill_in "song name", with: "La Terre est Ronde"
+    click_link "Add Song"
+    fill_in "name", with: "La Terre est Ronde"
     click_button "Continue"
-    full_in "artist", with: "Orelsan"
+    fill_in "artist", with: "Orelsan"
     click_button "Continue"
     click_button "Check for song on Spotify"
     sleep(2)
