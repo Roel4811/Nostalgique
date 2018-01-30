6a3c1b9b1790ddedabe6b90cb96ac52e90f1ce8e
Roel de Jong <Roel4811@hotmail.com>
2018-01-30 23:31:51 +0100

    Remove image upload

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,13 +1,12 @@
 class Song < ApplicationRecord
   validates :name, presence: true
   validates :lyrics, presence: true
+  validates :image_url, presence: true
 
   belongs_to :artist
   belongs_to :member
 
   enum status: [:pending, :active]
-
-  mount_uploader :image, ImageUploader
 
   def current_step
     @current_step || Wizard::Song::STEPS.first
