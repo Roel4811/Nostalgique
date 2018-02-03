149d39ab3542dcf6a211484dbdaeb7d5cb105e01
Roel de Jong <Roel4811@hotmail.com>
2018-02-03 22:41:15 +0100

    Update foctory

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,6 +9,7 @@ FactoryGirl.define do
   factory :song do
     name "test_song"
     lyrics "lalalyrics"
+    image_url "https://i.scdn.co/image/9f9d73b1c374fdcdb46b18a987790456e49f0720"
   end
 end
 
