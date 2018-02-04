b3f31111b14d8b7ac99be168d932025f0a2e4185
Roel de Jong <Roel4811@hotmail.com>
2018-02-04 18:53:41 +0100

    Add new song test

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe "AdminSongs", type: :request do
     visit admin_songs_path
     page.should have_content(song.name)
   end
+
+  it "adds a new song", type: :request do
+    visit new_admin_song_path
+    fill_in "Name", with: "La terre est ronde"
+    fill_in "Artist", with: "Orelsan"
+    fill_in "Member", with: "Roel"
+    fill_in "Lyrics", with: "au fond, je croix que la terre est rond... laala"
+    fill_in "Lyrics en", with: "sincerely, I believe that the world is round... lala"
+    click_button "Create Song"
+    page.should have_content("La terre est ronde")
+  end
 end
