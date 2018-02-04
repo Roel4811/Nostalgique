d5decd8580c2ea24c9b736387c1588bd96e108f2
Roel de Jong <Roel4811@hotmail.com>
2018-02-04 18:51:58 +0100

    Add create and update actions

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SongsController < Admin::ApplicationController
-  before_action :set_song, only: [:show]
+  before_action :set_song, only: [:show, :edit, :update]
 
   def index
     @songs = Song.search(params[:search])
@@ -13,12 +13,46 @@ class Admin::SongsController < Admin::ApplicationController
   end
 
   def new
+    @song = Song.new
   end
 
   def create
+    song = Song.new(song_params)
+    artist = Artist.find_or_create_by(name: artist_params[:artist])
+    song.artist = artist
+    if song.save!
+      redirect_to admin_root_path
+      flash[:notics] = "Song successfully created!"
+    else
+      redirect_to new_admin_song_path
+      flash[:warning] = "Something went wrong"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    artist = Artist.find_or_create_by(name: artist_params[:artist])
+    @song.artist = atist
+    if @song.update!(song_params)
+      redirect_to admin_root_path
+      flash[:notics] = "Song successfully updated!"
+    else
+      redirect_to edit_admin_song_path(@song)
+      flash[:warning] = "Something went wrong"
+    end
   end
 
   private
+
+  def song_params
+    params.require(:song).permit(:name, :member_id, :image_url, :lyrics, :lyrics_en)
+  end
+
+  def artist_params
+    params.require(:song).permit(:artist)
+  end
 
   def set_song
     @song = Song.find(params[:id])
