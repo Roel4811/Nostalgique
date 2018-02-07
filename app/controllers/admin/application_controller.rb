615aaf99b346664757c7812ced3cd97fb116b045
Roel de Jong <Roel4811@hotmail.com>
2018-02-07 22:03:25 +0100

    Update authorize actiion

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,12 +1,11 @@
 class Admin::ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # before_action :authorize
+
   layout 'admin'
-  
+
   def home
-    if !current_user
-      render 'sign_in'
-    end
   end
 
   private
@@ -15,5 +14,10 @@ class Admin::ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def authorize
+    session[:return_to] = request.fullpath
+    redirect_to log_in_admin_users_path unless current_user
+  end
 
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,33 @@
 class Admin::UsersController < Admin::ApplicationController
 
-  def sign_in
+  def log_in
+  end
+
+  def create_log_in
     user = User.find_by(email: sign_in_params[:email], deleted_at: nil)
 
     if user && user.authenticate(sign_in_params[:password])
       session[:user_id] = user.id
+      url = session[:return_to] || root_path
+      session[:return_to] = nil
+      url = admin_root_path if url.eql?(log_out_admin_users_path)
       flash[:notice] = 'You are looged in'
-      redirect_to admin_root_path
+      redirect_to url
     else
       flash[:error] = 'Something went wrong'
-      redirect_to admin_root_path
+      redirect_to log_in_admin_users_path
     end
   end
 
+  def log_out
+    session[:member_id] = nil
+    redirect_to log_in_admin_users_path
+  end
+
+
   private
 
-  def sign_in_params
+  def log_in_params
     params.require(:user).permit(:email, :password)
   end
 end
