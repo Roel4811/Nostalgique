e8486abb323b9a6a8b7f2c27e854591c16616da1
Roel de Jong <Roel4811@hotmail.com>
2018-02-11 23:54:13 +0100

    Add methods to turn params into dynamic methodnames

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -20,4 +20,25 @@ class Admin::ApplicationController < ActionController::Base
     redirect_to log_in_admin_users_path unless current_user
   end
 
+  def show_or_hide_deleted
+    if params[:show_or_hide_deleted] == "yes"
+      "only_deleted"
+    elsif params[:show_or_hide_deleted] == "no"
+      "without_deleted"
+    else
+      "all"
+    end
+  end
+  helper_method :show_or_hide_deleted
+
+  def sort_column
+    'id'
+  end
+  helper_method :sort_column
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+  end
+  helper_method :sort_direction
+
 end
