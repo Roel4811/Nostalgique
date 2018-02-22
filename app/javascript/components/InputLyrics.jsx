02981b33d8134c43dd98bf0da344521d24241f21
Roel de Jong <Roel4811@hotmail.com>
2018-02-22 22:42:32 +0100

    Update lyrics components

--- app/javascript/components/InputLyrics.jsx
+++ app/javascript/components/InputLyrics.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class InputLyrics extends React.Component {
-  constructor() {
-    super();
+  saveAndContinue(e) {
+    e.preventDefault();
+
+    var data = {
+      lyrics: this.refs.lyrics.value
+    }
+
+    this.props.saveValues(data);
+    this.props.nextStep();
+  }
+  goBack(e) {
+    e.preventDefault();
+
+    this.props.previousStep();
   }
   render() {
+    return (
+      <div className="add-song-card">
+        <h2>Let's add a song!</h2>
+        <label>What are the lyrics of the Song (in French)?</label>
+        <input className="string required" type="text" ref="lyrics" defaultValue={this.props.songValues.lyrics}/>
 
+        <button className="button-standard" onClick={(e) => this.goBack(e)}>Back</button>
+        <button className="button-standard" onClick={(e) => this.saveAndContinue(e)}>Save and Continue</button>
+      </div>
+    )
   }
 }
 
--- app/javascript/components/InputLyricsEnglish.jsx
+++ app/javascript/components/InputLyricsEnglish.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class InputLyricsEnglish extends React.Component {
-  constructor() {
-    super();
+  saveAndContinue(e) {
+    e.preventDefault();
+
+    var data = {
+      song_lyrics_en: this.refs.lyrics_en.value
+    }
+
+    this.props.saveValues(data);
+    this.props.nextStep();
+  }
+  goBack(e) {
+    e.preventDefault();
+
+    this.props.previousStep();
   }
   render() {
+    return (
+      <div className="add-song-card">
+        <h2>Let's add a song!</h2>
+        <label>What is the English translation of the lyrics?</label>
+        <input className="string required" type="text" ref="lyrics_en" defaultValue={this.props.songValues.lyrics_english}/>
 
+        <button className="button-standard" onClick={(e) => this.goBack(e)}>Back</button>
+        <button className="button-standard" onClick={(e) => this.saveAndContinue(e)}>Save and Continue</button>
+      </div>
+    )
   }
 }
 
