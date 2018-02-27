8fcb3a38161db7af12fd6eaea8d200122516b39b
Roel de Jong <Roel4811@hotmail.com>
2018-02-27 23:04:26 +0100

    Add confirmation component

--- app/javascript/components/AddSongConfirmation.jsx
+++ app/javascript/components/AddSongConfirmation.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 class AddSongConfirmation extends React.Component {
-  constructor() {
-    super();
+  addMessage() {
+    render(<h1>Thank you for contributing!</h1>, document.getElementById('message'));
   }
   render() {
-
+    return (
+      <div>
+        {this.addMessage()}
+        <p>The song {this.props.songValues.song_name} by {this.props.songValues.artist_name} will be reviewed and put on the website soon!</p>
+        <p>Do you want to receive an email when the song is available?</p>
+        <label>
+          <input type="checkbox"/>
+          Yes please!
+        </label>
+        <a href="/" className="button-standard">Go back to homepage</a>
+      </div>
+    )
   }
 }
 
