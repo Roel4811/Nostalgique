46dc3dbc82c0395232bccb30d2857033e4c8630e
Roel de Jong <Roel4811@hotmail.com>
2018-02-27 23:06:23 +0100

    Add link to songcard

--- app/javascript/components/SongCard.jsx
+++ app/javascript/components/SongCard.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const SongCard = ({song}) => {
   return (
-    <div className="song-card">
-      <div className="song-text">
-        <h3>{song.name}</h3>
-        <p>{song.lyrics.substring(0, 100)}</p>
+    <a className="song-card-link" href={'/songs/' + song.id}>
+      <div className="song-card">
+        <div className="song-text">
+          <h3>{song.name}</h3>
+          <p>{song.lyrics.substring(0, 100)}</p>
+        </div>
+        <div className="song-image">
+          <img src={song.image_url} alt="song-image"/>
+        </div>
       </div>
-      <div className="song-image">
-        <img src={song.image_url} alt="song-image"/>
-      </div>
-    </div>
+    </a>
   )
 }
 
