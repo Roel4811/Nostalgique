8ab1809793bfe816301c11fec031bf7303d8a886
Roel de Jong <Roel4811@hotmail.com>
2018-02-28 13:27:30 +0100

    Use react router

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import SongCards from '../components/SongCards';
 import SongForm from '../components/SongForm';
 import TestComponent from '../components/TestComponent';
+import AddSongConfirmation from '../components/AddSongConfirmation';
 // import NotFound from '../components/NotFound';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById('app');
-  render(<Router history={hashHistory}>
-           <Route path="/" component={SongCards}>
-             <IndexRoute component={Songs} />
-             <Route path="artists" component={Artists} />
-             <IndexRoute path="add song" component={Songs} />
-           </Route>
-         </Router>, app
-  )
-  // const songs_node = document.getElementById('songs_data');
-  // const new_song_node = document.getElementById('user_data');
-  // if (songs_node) {
-  //  const data = JSON.parse(songs_node.getAttribute('data'));
-  //  render(<SongCards songs={data} />, document.querySelector('#song_cards'));
-  // }
-  // if (new_song_node) {
-  //  const data = JSON.parse(new_song_node.getAttribute('data'));
-  //  render(<SongForm user={data} />, document.querySelector('#add_song_card'));
-  // }
+
+  const jsContent = document.getElementById('js-content');
+  render(<BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={SongCards} />
+            <Route path="/songs/new" component={SongForm} />
+            <Route path="/songs/confirm" component={AddSongConfirmation} />
+          </Switch>
+         </BrowserRouter>,
+        jsContent)
+
 });
