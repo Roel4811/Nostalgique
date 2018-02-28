71bfef0daae13cfbacdaf516de92e78afc09eab7
Roel de Jong <Roel4811@hotmail.com>
2018-02-28 21:21:10 +0100

    Add routes

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -6,18 +6,19 @@ import SongCards from '../components/SongCards';
 import SongForm from '../components/SongForm';
 import TestComponent from '../components/TestComponent';
 import AddSongConfirmation from '../components/AddSongConfirmation';
+import ContactForm from '../components/ContactForm';
+import ThankYou from '../components/ThankYou';
 // import NotFound from '../components/NotFound';
 
-document.addEventListener("DOMContentLoaded", () => {
-
-  const jsContent = document.getElementById('js-content');
-  render(<BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={SongCards} />
-            <Route path="/songs/new" component={SongForm} />
-            <Route path="/songs/confirm" component={AddSongConfirmation} />
-          </Switch>
-         </BrowserRouter>,
-        jsContent)
-
-});
+const jsContent = document.getElementById('js-content');
+render(<BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={SongCards} />
+          <Route exact path="/songs" component={SongCards} />
+          <Route path="/songs/new" component={SongForm} />
+          <Route path="/songs/confirm" component={AddSongConfirmation} />
+          <Route path="/contact" component={ContactForm} />
+          <Route path="/thank-you" component={ThankYou} />
+        </Switch>
+       </BrowserRouter>,
+      jsContent)
