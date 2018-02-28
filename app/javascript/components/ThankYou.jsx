7a6dbe158624f4f3fda66f59cdf670b0662cd692
Roel de Jong <Roel4811@hotmail.com>
2018-02-28 21:21:17 +0100

    Add thank you component

---
+++ app/javascript/components/ThankYou.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ThankYou extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Thank You!</h1>
+      </div>
+    )
+  }
+}
+
+export default ThankYou
