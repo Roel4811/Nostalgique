5e5a93c78723961700049ecf626e8721f18185f2
Roel de Jong <Roel4811@hotmail.com>
2018-02-28 13:26:33 +0100

    Parse data at component level

--- app/javascript/components/SongCards.jsx
+++ app/javascript/components/SongCards.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import SongCard from '../components/SongCard';
+import { Link } from 'react-router-dom';
 
-const SongCards = ({songs}) => {
+const SongCards = () => {
+  const songs_node = document.getElementById('songs_data');
+  const songs = JSON.parse(songs_node.getAttribute('data'));
   return (
     <section className="song-cards">
       <div className="container">
