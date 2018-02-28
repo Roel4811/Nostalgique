f5434be75880aa9793b59f48001a89a9b36e501e
Roel de Jong <Roel4811@hotmail.com>
2018-02-28 13:25:55 +0100

    Update song confirmation component

--- app/javascript/components/AddSongConfirmation.jsx
+++ app/javascript/components/AddSongConfirmation.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
 
 class AddSongConfirmation extends React.Component {
-  addMessage() {
-    render(<h1>Thank you for contributing!</h1>, document.getElementById('message'));
-  }
   render() {
     return (
       <div>
-        {this.addMessage()}
+        <h1>Thank you for contributing!</h1>
         <p>The song {this.props.songValues.song_name} by {this.props.songValues.artist_name} will be reviewed and put on the website soon!</p>
         <p>Do you want to receive an email when the song is available?</p>
         <label>
           <input type="checkbox"/>
           Yes please!
         </label>
-        <a href="/" className="button-standard">Go back to homepage</a>
+        <Link to="/" className="button-standard">Go back to homepage</Link>
       </div>
     )
   }
