f832ad7bdc02c7d64fb1afb7ccbb95973fab1245
Roel de Jong <Roel4811@hotmail.com>
2018-03-04 21:16:40 +0100

    Add artist show action

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,18 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: :show
 
   def index
     @artists = Artist.order('lower(name)')
+  end
+
+  def show
+    @songs = @artist.songs
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 
 end
