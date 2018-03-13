193bf5139deecfd5456d90d1959f01774083a863
Roel de Jong <Roel4811@hotmail.com>
2018-03-13 18:32:11 +0100

    Add tail log task

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,16 @@ namespace :puma do
   before :start, :make_dirs
 end
 
+desc "tail production log files"
+task :tail_logs, :roles => :app do
+  trap("INT") { puts 'Interupted'; exit 0; }
+  run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+    puts  # for an extra line break before the host name
+    puts "#{channel[:host]}: #{data}"
+    break if stream == :err
+  end
+end
+
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
