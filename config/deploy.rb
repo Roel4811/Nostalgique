253851e68a2d27c618078a08bba77718c7a9969b
Roel de Jong <Roel4811@hotmail.com>
2018-03-13 23:36:03 +0100

    Add deploy hook

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,8 +31,8 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/secrets.yml.key}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -83,5 +83,3 @@ end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-
-# append :linked_files, "config/secrets.yml.key"
