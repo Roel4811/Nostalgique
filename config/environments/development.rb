5f8b02cd65f4db8fa3a6a2ce2579df85192308b4
Roel de Jong <Roel4811@hotmail.com>
2018-03-19 23:18:34 +0100

    Auto files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+  config.webpacker.check_yarn_integrity = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # added to enable javascript_pack_tag to load assets from webpack-dev-server
